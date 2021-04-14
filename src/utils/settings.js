89bdb4ec43989f19d75eb10f6027be44b5fb00d7
gpichardo <gpichardob@gmail.com>
2021-04-14 21:05:06 +0200

    Update utils

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,7 +1,31 @@
 /**
+ * Internal dependencies
+ */
+import icons from './icons';
+
+/**
  * WordPress dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { applyFilters } from '@wordpress/hooks';
+
+export const ALIGNMENTS = [
+	{
+		icon: icons.alignLeft,
+		title: __( 'Align text left', 'gp-block-text-align-mobile' ),
+		align: 'left',
+	},
+	{
+		icon: icons.alignCenter,
+		title: __( 'Align text center', 'gp-block-text-align-mobile' ),
+		align: 'center',
+	},
+	{
+		icon: icons.alignRight,
+		title: __( 'Align text right', 'gp-block-text-align-mobile' ),
+		align: 'right',
+	},
+];
 
 /**
  * Check if given block is restricted.
@@ -10,17 +34,16 @@ import { applyFilters } from '@wordpress/hooks';
  * @return {boolean} true or false.
  */
 export function isRestrictedBlock( blockName ) {
+	const restrictedBlocks = applyFilters(
+		'gp-block-text-align-mobile.restricted-blocks',
+		[
+			'core/block',
+			'core/freeform',
+			'core/shortcode',
+			'core/template',
+			'core/nextpage',
+		]
+	);
 
-  const restrictedBlocks = applyFilters(
-    'gp-block-text-align-mobile.restricted-blocks',
-    [
-      'core/block',
-      'core/freeform',
-      'core/shortcode',
-      'core/template',
-      'core/nextpage',
-    ]
-  );
-
-  return restrictedBlocks.includes( blockName );
+	return restrictedBlocks.includes( blockName );
 }
